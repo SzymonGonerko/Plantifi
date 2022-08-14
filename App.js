e4ddf6716ecde8b489110b6e183f8f7a85a8948c
SzymonGonerko <szymon.gonerko@gmail.com>
2022-08-14 16:42:03 +0200

    Welcome

--- App.js
+++ App.js
@@ -1,13 +1,53 @@
+import * as SplashScreen from 'expo-splash-screen';
+import * as Font from 'expo-font';
+
+import { useEffect, useCallback, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+
+import { Welcome } from './screens/Welcome';
+import { Second } from './screens/Second';
+
+import NunitoItalic from "./assets/fonts/NunitoItalic.ttf"
+import NunitoBold from "./assets/fonts/NunitoBold.ttf"
+import NunitoBoldItalic from "./assets/fonts/NunitoBoldItalic.ttf"
+
+SplashScreen.preventAutoHideAsync();
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [appIsReady, setAppIsReady] = useState(false);
+  
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await Font.loadAsync({NunitoItalic, NunitoBold, NunitoBoldItalic});
+        await new Promise(resolve => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
+  return <>
+  <StatusBar style='light'/>
+  <SafeAreaView style={styles.container} onLayout={onLayoutRootView}>
+    <Welcome/>
+  </SafeAreaView>
+  </>
 }
 
 const styles = StyleSheet.create({
---
+++ screens/Second.js
@@ -0,0 +1,8 @@
+import { StyleSheet, Text, View } from 'react-native';
+export const Second = () => {
+    return <>
+    <View>
+        <Text>Second!</Text>
+    </View>
+    </>
+}
