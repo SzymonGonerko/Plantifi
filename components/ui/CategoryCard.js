1ed7d5a9c81f98e293315a762b024b7580df226f
SzymonGonerko <szymon.gonerko@gmail.com>
2022-09-07 21:44:15 +0200

    CategoryCard animation

--- components/ui/CategoryCard.js
+++ components/ui/CategoryCard.js
@@ -1,61 +1,107 @@
-import { useState } from 'react';
-import { StyleSheet, Text, View, ImageBackground, Dimensions, Image, Pressable, Animated, FlatList} from 'react-native';
-import { plantsCategory } from '../../plantsData';
-
-import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons"
+import { useRef, useEffect } from 'react';
+import { StyleSheet, View, ImageBackground, Pressable, Animated} from 'react-native';
 
 
 export const CategoryCard = ({src, name, onPress, index, isSelected}) => {
-    
-    
+    const defaultText = useRef(new Animated.Value(1)).current;
+    const selectedText = useRef(new Animated.Value(0)).current;
 
+    useEffect(() => {
+        if (!isSelected) fadeOut()
+    }, [isSelected])
+    
+    const fadeIn = () => {
+        Animated.timing(selectedText, {
+          toValue: 1,
+          duration: 350,
+          useNativeDriver: true
+        }).start();
+        Animated.timing(defaultText, {
+            toValue: 0,
+            duration: 350,
+            useNativeDriver: true
+          }).start();
+      };
+    
+      const fadeOut = () => {
+        Animated.timing(selectedText, {
+            toValue: 0,
+            duration: 350,
+            useNativeDriver: true
+          }).start()
+          Animated.timing(defaultText, {
+            toValue: 1,
+            duration: 350,
+            useNativeDriver: true
+          }).start();
+      };
 
     return <>
-    <View style={{flexDirection: "column", alignItems: "center"}}>
-        <View style={[styles.container, (isSelected? styles.selectedContainer: null)]}>
+    <View style={styles.container}>
+        <View style={[styles.squareContainer, (isSelected && styles.selectedSquareContainer)]}>
             <Pressable 
             style={({pressed}) => pressed ? [styles.buttonInnerContainer, styles.pressed] : [styles.buttonInnerContainer]}
-            onPress={() => onPress(index)} android_ripple={{color: "#9BA9BC"}}>
+            onPress={() => (onPress(index), fadeIn())} android_ripple={{color: "#9BA9BC"}}>
 
                 <ImageBackground source={src} resizeMode="contain" imageStyle={styles.imgs} style={styles.imgs}/>
 
             </Pressable>
         </View>
-        <Text style={[styles.tagText, (isSelected? styles.selectedText: null)]}>{name}</Text>
-    </View>
 
-    
-    
+        <View style={styles.tagTextContainer}>
+            <Animated.Text style={[styles.tagText, {opacity: defaultText}]}>{name}</Animated.Text>
+            <Animated.Text style={[styles.selectedTagText, {opacity: selectedText}]}>{name}</Animated.Text>
+        </View>
+        
+    </View>
     </>
 }
 
 const styles = StyleSheet.create({
+    container: {
+        flexDirection: "column",
+        alignItems: "center",
+        width: 90,
+        height: 80
+    },
+    tagTextContainer: {
+        position: "relative", 
+        width: "100%", 
+        height: "80%"
+    },
     tagText: {
+        position: 'absolute',
         fontSize: 12,
         fontFamily: "NunitoRegular",
         textAlign: "center",
-        maxWidth: 70,
-        marginRight: 10,
-        marginLeft: 20,
-        lineHeight: 13
+        lineHeight: 13,
+        width: "100%",
     },
-    container: {
-        position: 'relative',
+    selectedTagText: {
+        position: 'absolute',
+        fontSize: 13,
+        fontFamily: "NunitoBold",
+        textAlign: "center",
+        lineHeight: 13,
+        width: "100%",
+        color: "#54795E"
+    },
+    squareContainer: {
         justifyContent: "center",
         alignItems: "center",
         flexDirection: "column",
         width: 50,
         height: 50,
-        marginRight: 10,
         backgroundColor: "white",
         borderRadius: 8,
         marginBottom: 6,
-        marginLeft: 20,
         overflow: 'hidden',
-        
     },
-    selectedContainer: {
-        elevation: 4
+
+    selectedSquareContainer: {
+        elevation: 4,
+        borderWidth: 1,
+        borderColor: "#54795E"
     },
     imgs: {
         position: 'absolute',
@@ -79,8 +125,4 @@ const styles = StyleSheet.create({
     pressed: {
         opacity: 0.75,
     },
-    selectedText: {
-        color: "green",
-        fontFamily: "NunitoBold",
-    }
 })
